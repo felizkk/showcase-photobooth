ea2f6d3a43aa183b993da65d708b475d69db6f6f
Feliz <felizkk@yahoo.com>
2019-03-18 11:09:40 +1300

    update

--- script/unique_id.sh
+++ script/unique_id.sh
@@ -1,4 +1,4 @@
 timestamp=`date '+%d%m%H%M%S'`
-firstname=`echo "$1 $2" | awk '{print tolower($1)}'`
-uniqueid=`echo $firstname-$timestamp`
+shortname=`echo "$1 $2" | awk '{printf("%s%s\n",substr($1,1,2),substr($2,1,2))}'`
+uniqueid=`echo $shortname-$timestamp`
 echo "$uniqueid"
