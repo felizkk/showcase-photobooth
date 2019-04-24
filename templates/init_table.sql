dea2b1d37d5089b3585889e0c33af98b22bbdcdd
Feliz <felizkk@yahoo.com>
2019-04-24 13:39:10 +1200

    update

--- templates/init_table.sql
+++ templates/init_table.sql
@@ -6,10 +6,11 @@ CREATE TABLE `visitor` (
   `fullname` varchar(100) NOT NULL,
   `company` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `timestamp` varchar(100),
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=11001 DEFAULT CHARSET=latin1;
 
 LOCK TABLES `visitor` WRITE;
-INSERT INTO `visitor` (`location`,`external_id`,`fullname`,`company`,`email`) VALUES ('sc-akl','feliz-1203155601','Feliz Karnadi','Ingram Micro NZ', 'feliz.karnadi@ingrammicro.com');
-INSERT INTO `visitor` (`location`,`external_id`,`fullname`,`company`,`email`) VALUES ('sc-akl','grant-1203155602','Grant Du Preez','Ingram Micro NZ', 'grant.dupreez@ingrammicro.com');
+INSERT INTO `visitor` (`location`,`external_id`,`fullname`,`company`,`email`) VALUES ('sc-akl','feliz-1203155601','Feliz Karnadi','Ingram Micro NZ', 'feliz.karnadi@ingrammicro.com', '1556068678');
+INSERT INTO `visitor` (`location`,`external_id`,`fullname`,`company`,`email`) VALUES ('sc-akl','grant-1203155602','Grant Du Preez','Ingram Micro NZ', 'grant.dupreez@ingrammicro.com', '1556068678');
 UNLOCK TABLES;
